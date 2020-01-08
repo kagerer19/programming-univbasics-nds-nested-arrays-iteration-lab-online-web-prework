c1ecbd0d58f51976d31391d98afd80244bb2c30e
Alexander kagerer <kagerer19@gmail.com>
2020-01-08 12:42:56 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -4,7 +4,7 @@ def join_ingredients(src)
       ["peperoni, sausage"],
       ["green olives, green pepers"],
       ["onions, pineapple"]
-      
+    end
        ]
        
   # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
